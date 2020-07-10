ad8a433daffd7dc3e92edbd19d3c2fbb4136b63b
Minakshi Verma <mvedwan@gmail.com>
2020-07-10 12:12:03 -0400

    ex4 tests are passing!

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,24 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Check if there are numbers <0 in the list
+    # store them in cache as negative numbers
+    # Check the given list and match with cache
+    # if number found return it
+
+    cache ={}
+    for number in a:
+        # check for negatives
+        if number < 0:
+            # store them in cache as negative number
+            cache[number * -1] = True
+    result = []
+
+    #Iterate through the whole list of number
+    for number in a:
+        # if number matches with numbers stored in cache, means it has a pair
+        if number in cache:
+            result.append(number) 
 
     return result
 
